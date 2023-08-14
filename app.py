7d2474465ac0e4928947925c39e0e5f447c99b9a
Abdullah Alam <abdullahalam@protonmail.com>
2023-08-13 22:17:14 -0400

    use prompt engineering and code to remove extraneous stuff from responses

--- app.py
+++ app.py
@@ -19,20 +19,47 @@ openai.api_key = api_key
 def index():
     return render_template('index.html')
 
+# remove extraneous stuff from response
+def parse_output(response):
+    if '```' in response:
+        op_lines = []
+        lines = str(response).split('\n')
+        begun = False
+
+        for line in lines:
+
+            if begun  and '```' not in line:
+                op_lines.append(line)
+
+            if '```' in line:
+                begun = True
+
+        code_op = '\n'.join(op_lines)
+        return code_op
+    else:
+        return response
+
+
+
 # endpoint for generating responses
 @app.route('/get_response', methods=['POST'])
 def get_response():
     data = request.json
     prompt = data.get('prompt', '')
-    response = openai.Completion.create(
-        engine="text-davinci-003",
-        prompt=prompt,
-        max_tokens=150
-    )
-    chatbot_response = response.choices[0].text.strip()
 
-    print(chatbot_response)
-    return jsonify({'response': chatbot_response})
+    completions = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[
+            {"role": "system", "content": "You are a helpful assistant who writes code. Your responses will contain code and only code, and nothing else. Do not give an introduction or explanation. Do not show any outputs. Give one and only one code block."},
+            {"role": "user", "content": "You are a helpful assistant who writes code. Your responses will contain code and only code, and nothing else. Do not give an introduction or explanation. Do not show any outputs. Give one and only one code block."},
+            {"role": "user", "content": prompt},
+        ]
+    )
+
+    response = completions['choices'][0]['message']['content']
+    processed = parse_output(response)
+
+    return jsonify({'response': processed})
 
 if __name__ == '__main__':
     app.run(debug=True)
